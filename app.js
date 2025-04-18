bf7d7c46a2bae433fa44aa69df3c701ea9018d88
Milind Sangale <smilind900@gmail.com>
2025-04-19 01:13:50 +0530

    Project Phase-1 Part(a) Update/Delete Route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app= express();
 const port = 3000;
 const Listing = require('./models/listing.js');
 const path = require('path');
+const methodOverride = require('method-override');
 
 main()
 .then(() => {
@@ -20,6 +21,7 @@ async function main() {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.send('root is working!')
@@ -54,6 +56,27 @@ app.post("/listings", async (req, res) => {
 });
 
 
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 
 
 /* 
