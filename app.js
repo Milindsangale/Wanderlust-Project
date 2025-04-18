b21ca4114ca7325df4fb26ebc7a076d95f775d68
Milind Sangale <smilind900@gmail.com>
2025-04-19 00:57:43 +0530

    Project Phase-1 Part(a) new Route

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ async function main() {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-  
+app.use(express.urlencoded({extended:true}));
+
 app.get('/', (req, res) => {
     res.send('root is working!')
   
@@ -31,6 +32,28 @@ app.get("/listings", async (req, res) => {
     const alllistings = await Listing.find({});
     res.render("listings/index", {alllistings});
 });
+ 
+// New Route
+app.get("/listings/new", async (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+// create route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+  console.log("New listing created:", newListing);
+});
+
+
 
 
 /* 
