294eb931b1b36b0be31e0527cb1085467d1829ff
Milind Sangale <smilind900@gmail.com>
2025-04-19 14:00:09 +0530

    Project Phase-1 Part(b) style

--- models/listing.js
+++ models/listing.js
@@ -14,8 +14,8 @@ const listingSchema = new Schema({
     },
     image:{
         type:String,
-        default: "https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        set: (v) => v === "" ? "https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" : v, // Set to undefined if empty string
+        default: "https://images.pexels.com/photos/221471/pexels-photo-221471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        set: (v) => v === "" ? "https://images.pexels.com/photos/221471/pexels-photo-221471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" : v, // Set to undefined if empty string
     },
     price:{
         type:Number,
